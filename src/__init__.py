c86eff527622a57bda097dd6cd49b15cd2d3a9e6
KUN <leeguandon@gmail.com>
2025-03-31 00:28:04 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,9 +3,7 @@ from .hyperspectral.feathernet3d import FeatherNet_network
 from .hyperspectral.dgcnet import DGCdenseNet
 from .hyperspectral.lgcnet import LgcdenseNet
 from .hyperspectral.dydensenet import DydenseNet
-from .hyperspectral.densenet_unet3d import Densenet_Unet3D
 from .hyperspectral.codensenet import CodenseNet
-from .hyperspectral.dsdensenet import DsdenseNet
 
 from .utils.loading import load_dataset, sampling
 from .utils.utils import generate_iter, aa_and_each_accuracy, generate_png
